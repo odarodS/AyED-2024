34483251717547a5395b3ec36e0e19afd91f9ca8
odarodS <167914611+odarodS@users.noreply.github.com>
2024-05-23 14:57:37 -0300

    1_Dorado_LaSumaAnterior

---
+++ etapa1/1_Dorado_LaSumaAnterior/1_Dorado_LaSumaAnterior/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1_Dorado_LaSumaAnterior
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        {
+                /*Crear 3 variables numéricas con el valor que tu quieras y
+                 *  en otra variable numérica guardar el valor de la suma
+                 *   de las 3 anteriores. 
+                 *   Mostrar por consola. */
+                int numero1 = 5;
+                int numero2 = 10;
+                int numero3 = 15;
+                int sumadeTODOS = (numero1 + numero2 + numero3);
+                Console.WriteLine("la suma de los numeros es " + sumadeTODOS);
+                Console.ReadKey();
+
+            
+
+            }
+        }
+    }
+
+}
+
+
