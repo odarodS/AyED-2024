95e0d22d33eb8e3feb7ed768673ada36655a6c12
odarodS <167914611+odarodS@users.noreply.github.com>
2024-08-13 16:15:58 -0300

    Dorado_0_MiPrimerFuncion

---
+++ Etapa3/Dorado_0_MiPrimerFuncion/Dorado_0_MiPrimerFuncion/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dorado_0_MiPrimerFuncion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { /*Escribir una función a la que se le pase una cadena <nombre> 
+            y
+            muestre por pantalla el saludo ¡hola <nombre>!*/
+            saludar("carlos");
+            Console.ReadKey();
+
+        }
+        static void saludar(string nombre)
+        {
+            Console.WriteLine("hola " +  nombre);
+
+        }
+        
+    }
+}
