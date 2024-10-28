8e53de5a5ff55b38c0071cf3b1a7872da70fb2e0
odarodS <167914611+odarodS@users.noreply.github.com>
2024-10-28 19:04:23 -0300

    Add files via upload

---
+++ etapa1/Dorado_DiezVeces_13/Dorado_DiezVeces_13/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dorado_DiezVeces_13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Escribir un programa que pida al usuario una palabra y 
+             * la muestre por pantalla 10 veces.*/
+            Console.WriteLine("Ingrese una palabra");
+            string Palabra = Console.ReadLine();
+            int contador = 0;
+            while(contador <=  9) {
+                Console.WriteLine( "\n" + Palabra);
+                contador += 1;
+            }
+
+        Console.ReadKey();
+        }
+    }
+}
