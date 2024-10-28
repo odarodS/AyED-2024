aabe9f8da24a87b3d041da52aa6c06b092507d48
odarodS <167914611+odarodS@users.noreply.github.com>
2024-10-28 19:04:57 -0300

    Add files via upload

---
+++ etapa1/Dorado_Cumplidos_14/Dorado_Cumplidos_14/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dorado_Cumplidos_14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* Escribir un programa que pregunte al usuario su edad y 
+             * muestre por pantalla 
+             * todos los años que ha cumplido (desde 1 hasta su edad).*/
+            Console.WriteLine("decime tu edad");
+            int  edad = int.Parse(Console.ReadLine());
+            int edad2 = edad;
+            int contador = 0;
+            while (contador < edad) {
+                Console.WriteLine((edad2 - edad +1) );
+            edad2 += 1;
+            contador += 1;
+            }
+
+        Console.ReadKey();
+        }
+    }
+}
