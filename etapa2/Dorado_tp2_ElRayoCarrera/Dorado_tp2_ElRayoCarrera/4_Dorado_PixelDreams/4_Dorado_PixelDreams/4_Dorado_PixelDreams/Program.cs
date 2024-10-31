896ac6c0c48fc1a04cb280c9b562593aa3c0e436
odarodS <167914611+odarodS@users.noreply.github.com>
2024-10-31 14:29:10 -0300

    Add files via upload

---
+++ etapa2/Dorado_tp2_ElRayoCarrera/Dorado_tp2_ElRayoCarrera/4_Dorado_PixelDreams/4_Dorado_PixelDreams/4_Dorado_PixelDreams/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4_Dorado_PixelDreams
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* En la empresa de videojuegos "Pixel Dreams", 
+             * se está organizando un torneo en línea de su juego más 
+             * popular, "Galaxy Quest". Para determinar los ganadores, 
+             * necesitan un programa que ordene los puntajes de los 
+             * jugadores de mayor a menor.
+El programa solicitará al usuario que ingrese la cantidad de participantes
+en el torneo.Luego, pedirá al usuario que ingrese los puntajes de 
+cada participante, uno por uno.Una vez ingresados todos los puntajes, 
+el programa los ordenará de mayor a menor.Finalmente,
+mostrará la lista de puntajes ordenados, indicando el primer lugar 
+(mayor puntaje) y el último lugar (menor puntaje). */
+            Console.WriteLine("Ingrese la cantidad de participantes");
+            int CantParticipantes = int.Parse(Console.ReadLine());
+            int [] Participantes = new int[CantParticipantes];
+            for (int cont = 0; cont < Participantes.Count(); cont++)
+            {
+                Console.WriteLine("Ingrese el puntaje del participante N° " + (cont + 1));
+                int Puntaje = int.Parse(Console.ReadLine());
+                Participantes[cont] = Puntaje;
+                
+            }
+                for (int cont = 0; cont < CantParticipantes - 1 - cont; cont++)
+                { 
+                    for (int j = 0; j < CantParticipantes - 1 - cont; j++)
+                   {
+                    if (Participantes[j] < Participantes[j + 1])
+                    {
+                        
+                        int temp = Participantes[j];
+                        Participantes[j] = Participantes[j + 1];
+                        Participantes[j + 1] = temp; Console.ReadKey();
+                    }  
+                   }
+                }
+            Console.WriteLine("\nPuntajes ordenados de mayor a menor:");
+            for (int cont = 0; cont < CantParticipantes; cont++)
+            {
+                Console.WriteLine("Lugar " + (cont + 1) + " " + (Participantes[cont]) + "puntos");
+            }
+            Console.WriteLine("\n" + " Primer lugar: " + (Participantes[0]) + " puntos");
+            Console.WriteLine("Último lugar:" + (Participantes[CantParticipantes - 1]) + " puntos");
+            Console.ReadKey();
+        }
+    }
+}
+        
---
+++ etapa2/Dorado_tp2_ElRayoCarrera/Dorado_tp2_ElRayoCarrera/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dorado_tp2_ElRayoCarrera
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*El Rayo McQueen está compitiendo en una carrera de alta 
+             * velocidad. Necesita un programa en C# que le ayude a 
+             * registrar los 
+             * tiempos de cada vuelta y calcular algunas estadísticas 
+             * importantes. 
+
+El programa debe:
+Permitir a Rayo McQueen ingresar los tiempos de cada vuelta de la carrera 
+en segundos. Utiliza un array para almacenar estos tiempos.
+Calcular y mostrar en pantalla el tiempo total de la carrera.
+Calcular y mostrar en pantalla el promedio de tiempo por vuelta.
+Determinar y mostrar en pantalla cuál fue la mejor vuelta, es decir, 
+la vuelta con el menor tiempo registrado.
+El programa debe permitir al usuario ingresar la cantidad de vueltas 
+que completó Rayo McQueen y luego ingresar los tiempos de cada vuelta. 
+Al finalizar, mostrará las estadísticas calculadas. */
+            Console.WriteLine("ingrese la cantidad de vueltas");
+            int vueltas = int.Parse(Console.ReadLine());
+            int[] CantVueltas = new int[vueltas];
+            for (int cont = 0 ; cont < CantVueltas.Count(); cont++)
+            {
+                Console.WriteLine("Ingrese el tiempo de cada una de las vueltas " + (cont+1));
+                int tiempo  = int.Parse(Console.ReadLine());
+                CantVueltas[cont] = tiempo; 
+            }
+            int Total = 0;
+            int mejorvuelta = 100;
+            for (int cont = 0; cont < CantVueltas.Count(); cont++)
+            {
+
+                Total += CantVueltas[cont];
+               
+
+            }
+            for (int cont = 0; cont < CantVueltas.Count(); cont++)
+            {
+                if (CantVueltas[cont] < mejorvuelta)
+
+                {
+                    mejorvuelta = CantVueltas[cont];
+                }
+            }
+
+                Console.WriteLine("El tiempo total de la carrera es " + Total + " segundos");
+            Console.WriteLine(" La mejor vuelta por el menor tiempo fue " + mejorvuelta);
+            Console.WriteLine("El promedio de tiempo por vuelta es " + (Total / vueltas + " segundos"));
+                Console.ReadKey();
+        }
+    }
+}
