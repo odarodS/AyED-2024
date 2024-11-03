6ef0d6c0c3dc14d636012a58b763566733baf160
odarodS <167914611+odarodS@users.noreply.github.com>
2024-11-03 17:15:36 -0300

    Add files via upload

---
+++ etapa2/6_Dorado_ZEGARPG/6_Dorado_ZEGARPG/Etapa 2 (tp7 parriba)/7_Dorado_ContratacionSoft/7_Dorado_ContratacionSoft/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _7_Dorado_ContratacionSoft
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* Una empresa de desarrollo de software está en proceso de contratar un nuevo equipo para un proyecto importante. Durante el proceso de selección, necesitan un programa que ordene los puntajes obtenidos en un examen de menor a mayor, y luego filtre aquellos puntajes que sean múltiplos de un número dado.
+
+El programa solicitará al usuario que ingrese la cantidad de 
+candidatos que realizaron el examen.Luego, pedirá al usuario que 
+ingrese los puntajes de cada candidato, uno por uno.Una vez ingresados
+todos los puntajes, el programa los ordenará de menor a mayor.
+Posteriormente, el programa solicitará al usuario que ingrese 
+un número entero para filtrar los puntajes múltiplos de ese número.
+Finalmente, mostrará al usuario la lista de puntajes ordenados y
+los puntajes que son múltiplos del número especificado. */
+
+            Console.WriteLine(" Ingrese la cantidad de personas que realizaron el examen ");
+            int Examenes = int.Parse(Console.ReadLine());
+            int[] CantUsuarios = new int[Examenes];
+            Console.WriteLine(" Ingrese un numero para filtrar los puntajes multiplos de ese mismo numero ");
+            int Filtro = int.Parse(Console.ReadLine());
+            for (int Cont = 0; Cont < CantUsuarios.Count();Cont++ )
+            {
+                Console.WriteLine(" iNGRESE EL PUNTAJE DEL ALUMNO N°: "+ (Cont + 1));
+                int Puntaje = int.Parse(Console.ReadLine());
+                CantUsuarios[Cont] = Puntaje; 
+
+            }
+            for (int Cont = 0; Cont < CantUsuarios.Count() - 1; Cont++)
+            {
+                for (int j = 0; j < CantUsuarios.Count() - 1 -  Cont; j++)
+                {
+                    
+                    if (CantUsuarios[j] < CantUsuarios[j + 1])
+                    {
+                        int aux = CantUsuarios[j];
+                          CantUsuarios[j] = CantUsuarios[j + 1];
+                        CantUsuarios[j + 1] = aux;
+
+
+
+                    } 
+                   
+                }
+            }
+            for (int Cont = 0; Cont < CantUsuarios.Count(); Cont++)
+            {
+                Console.WriteLine(" Puntajes: "+ CantUsuarios[Cont] );
+                
+            }
+                Console.ReadKey();
+        }
+    }
+}
---
+++ etapa2/6_Dorado_ZEGARPG/6_Dorado_ZEGARPG/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _6_Dorado_ZEGARPG
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { /* La compañía de videojuegos "ZEGA" está desarrollando un nuevo
+            juego de rol (RPG) en el que los jugadores pueden encontrar 
+            objetos con diferentes valores. Necesitan un programa
+            que permita identificar en qué posición se encuentran los
+            objetos cuyo valor es mayor a un número dado.
+
+El programa solicitará al usuario que ingrese la cantidad de elementos 
+(objetos) que desea analizar.Luego, pedirá al usuario que ingrese el valor
+mínimo que deben tener los objetos para ser considerados en el filtro.
+Seguidamente, el programa solicitará al usuario que ingrese los valores de
+los objetos, uno por uno.Una vez ingresados todos los valores, 
+el programa mostrará al usuario las posiciones en las que se encuentran
+los objetos cuyo valor es mayor al número especificado. */
+
+            Console.WriteLine("Ingrese la cantidad de objetos que desea analizar");
+            int Objetos = int.Parse(Console.ReadLine());
+            int[] CantObjetos = new int[Objetos];
+            Console.WriteLine("Ingrese el valor minimo que deben tener los objetos para ser considerados en el filtro"); 
+            int ValorMinimo = int.Parse(Console.ReadLine());
+            for (int cont = 0; cont < CantObjetos.Count(); cont++ )
+            {
+                Console.WriteLine("Ingrese el Valor que debe tener el objeto N°: " + (cont + 1));
+                int ValorObjetos = int.Parse(Console.ReadLine());
+                if (ValorObjetos > ValorMinimo)
+                {
+                    CantObjetos[cont] = ValorObjetos;
+                }
+                
+            }
+            for (int cont = 0; cont < CantObjetos.Count(); cont++)
+            {
+                Console.WriteLine(" las posiciones son: " + CantObjetos[cont]);
+            }
+
+                Console.ReadKey();
+        }
+    }
+}
