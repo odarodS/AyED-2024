0ea62834c8e34ee2efa3e045aa98b45f72ac2440
odarodS <167914611+odarodS@users.noreply.github.com>
2024-11-03 17:58:53 -0300

    Add files via upload

---
+++ Etapa4/tp2_LoginSimple_Dorado/tp2_LoginSimple_Dorado/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace tp2_LoginSimple_Dorado
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string nombre = textBox1.Text;
+            string contraseña = textBox2.Text;
+            if (nombre == "Admin" &&( contraseña == "Admin12345"))
+            {
+                MessageBox.Show("Autorizado a ingresar");
+            }
+            else
+            {
+                MessageBox.Show("Usuario o contraseña incorrecta");
+            }
+        }
+    }
+}
