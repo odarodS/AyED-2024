e0725c64bed049272a10ded302258888c66148c4
odarodS <167914611+odarodS@users.noreply.github.com>
2024-11-03 17:23:36 -0300

    Add files via upload

---
+++ etapa2/Dorado_11_SumandoMatrices/Dorado_11_SumandoMatrices/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dorado_11_SumandoMatrices
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Sumar dos matrices de igual tamaño nxn.*/
+
+            int[,] matriz1 = new int[3, 3];
+            int[,] matriz2 = new int[3, 3];
+            int[,] suma = new int[3, 3];
+
+            Random aleatorio = new Random();
+
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    matriz1[i, j] = aleatorio.Next(1, 10);
+                }
+            }
+
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    Console.Write(matriz1[i, j] + "\t");
+                }
+                Console.WriteLine();
+            }
+
+            Console.WriteLine("                   +");
+
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    matriz2[i, j] = aleatorio.Next(1, 10);
+                }
+            }
+
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    Console.Write(matriz2[i, j] + "\t");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine("");
+
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    suma[i, j] = matriz1[i, j] + matriz2[i, j];
+                }
+            }
+
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    Console.Write(suma[i, j] + "\t");
+                }
+                Console.WriteLine();
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
