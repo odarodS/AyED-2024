5233e48767d889e19bd02492ee6dddd35a662cfd
odarodS <167914611+odarodS@users.noreply.github.com>
2024-11-03 17:56:18 -0300

    Add files via upload

---
+++ Etapa4/Tp0_Dorado/TrabajoDePrueba/Form1.Designer.cs
@@ -0,0 +1,146 @@
+﻿namespace TrabajoDePrueba
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label46 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.Farenheit = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.Kelvin = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label46
+            // 
+            this.label46.AutoSize = true;
+            this.label46.Location = new System.Drawing.Point(180, 72);
+            this.label46.Name = "label46";
+            this.label46.Size = new System.Drawing.Size(39, 13);
+            this.label46.TabIndex = 0;
+            this.label46.Text = "Kelvin:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(52, 72);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(164, 98);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(54, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Farenheit:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(221, 72);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(0, 13);
+            this.label3.TabIndex = 3;
+            // 
+            // Farenheit
+            // 
+            this.Farenheit.AutoSize = true;
+            this.Farenheit.Location = new System.Drawing.Point(221, 98);
+            this.Farenheit.Name = "Farenheit";
+            this.Farenheit.Size = new System.Drawing.Size(17, 13);
+            this.Farenheit.TabIndex = 4;
+            this.Farenheit.Text = "°F";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(52, 25);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(122, 13);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "Ingrese los grados en °C";
+            // 
+            // Kelvin
+            // 
+            this.Kelvin.AutoSize = true;
+            this.Kelvin.Location = new System.Drawing.Point(221, 72);
+            this.Kelvin.Name = "Kelvin";
+            this.Kelvin.Size = new System.Drawing.Size(18, 13);
+            this.Kelvin.TabIndex = 7;
+            this.Kelvin.Text = "°K";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(52, 123);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "PASAJE";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.Kelvin);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.Farenheit);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label46);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label46;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label Farenheit;
+        private System.Windows.Forms.Button CLICKEAME;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label Kelvin;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Etapa4/Tp0_Dorado/TrabajoDePrueba/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TrabajoDePrueba
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+           
+        }
+          private void button1_Click(object sender, EventArgs e)
+        {
+            double Num;
+            Num = double.Parse(textBox1.Text);
+
+            pas_kelvin(Num);
+            pas_farenheit(Num);
+        }
+        void pas_kelvin(double Num)
+        {
+            Kelvin.Text = Num + 273 + " °K";
+        }
+
+        void pas_farenheit(double Num)
+        {
+            Farenheit.Text = Num * 18 / 10 + 32 + " °F";
+        }
+
+      
+    }
+}
