84db51df22b4e9bdbf9924744d2fc1de8e669342
odarodS <167914611+odarodS@users.noreply.github.com>
2024-11-09 18:29:15 -0300

    7_Dorado_AlReves

---
+++ etapa1/7_Dorado_AlReves/7_Dorado_AlReves/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _7_Dorado_AlReves
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* Escriba un programa en C# que solicite 
+             * al usuario tres letras y los muestre al revés.*/
+            Console.WriteLine("ingrese SOLO 1 letra");
+            Char letra = Char.Parse(Console.ReadLine());
+            Console.WriteLine("ingrese otra letra");
+            Char letra2 = Char.Parse(Console.ReadLine());
+            Console.WriteLine("ingrese otra letra");
+            Char letra3 = Char.Parse(Console.ReadLine());
+            Console.WriteLine("Las letras AlReves son " + letra3 + letra2 + letra);
+            Console.ReadKey();
+        }
+    }
+}
